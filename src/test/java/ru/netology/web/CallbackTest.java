52dd3d3cc01d0427d4697f9126ca68f0fc90c477
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-08-04 22:08:42 +0300

    aaaa

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -31,7 +31,7 @@ public class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-//        driver.get("http://localhost:9999");
+        driver.get("http://localhost:9999");
 
     }
 
@@ -40,6 +40,20 @@ public class CallbackTest {
         driver.quit();
         driver = null;
     }
+
+
+    @Test
+    void secondTest() {
+//        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Елена Кузнецова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79150882118");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+
 
     @Test
     void firstTest() {
@@ -64,14 +78,5 @@ public class CallbackTest {
         assertEquals(expectedText.trim(), actualText.trim());
     }
 
-    @Test
-    void secondTest() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Елена Кузнецова");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79150882118");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
+
 }
