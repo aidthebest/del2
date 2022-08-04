114a05658a18840bd033f573f79ec5c9d1013a95
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-08-04 22:02:20 +0300

    aaaa

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -63,4 +63,15 @@ public class CallbackTest {
         String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expectedText.trim(), actualText.trim());
     }
+
+    @Test
+    void secondTest() {
+//        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Елена Кузнецова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79150882118");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 }
